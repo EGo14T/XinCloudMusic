b2a4875022f58265ee161453958d54c79557e2d4
EGo14T <974412906@qq.com>
2019-11-30 12:57:01 +0800

    oAuth2

--- xinmusic-oauth2/src/main/java/com/ego14t/oauth2/server/config/AuthorizationServerConfiguration.java
+++ xinmusic-oauth2/src/main/java/com/ego14t/oauth2/server/config/AuthorizationServerConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -14,41 +15,33 @@ import org.springframework.security.oauth2.provider.client.JdbcClientDetailsServ
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
-    @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        // 配置数据源（注意，我使用的是 HikariCP 连接池），以上注解是指定数据源，否则会有冲突
-        return DataSourceBuilder.create().build();
-    }
+    @Resource
+    private BCryptPasswordEncoder passwordEncoder;
 
-    @Bean
-    public TokenStore tokenStore() {
-        // 基于 JDBC 实现，令牌保存到数据
-        return new JdbcTokenStore(dataSource());
-    }
-
-    @Bean
-    public ClientDetailsService jdbcClientDetails() {
-        // 基于 JDBC 实现，需要事先在数据库配置客户端信息
-        return new JdbcClientDetailsService(dataSource());
-    }
-
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        // 设置令牌
-        endpoints.tokenStore(tokenStore());
-    }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         // 读取客户端配置
-        clients.withClientDetails(jdbcClientDetails());
+        //clients.withClientDetails(jdbcClientDetails());
+        clients
+                // 使用内存设置
+                .inMemory()
+                // client_id
+                .withClient("client")
+                // client_secret
+                .secret(passwordEncoder.encode("secret"))
+                // 授权类型
+                .authorizedGrantTypes("authorization_code")
+                // 授权范围
+                .scopes("app")
+                // 注册回调地址
+                .redirectUris("http://www.baidu.com");
     }
 }
