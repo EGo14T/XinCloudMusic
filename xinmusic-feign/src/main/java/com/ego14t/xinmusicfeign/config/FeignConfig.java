15ccd4614f90fd30c9751272d3ceb593577e0daf
EGo14T <974412906@qq.com>
2020-10-12 10:05:09 +0800

    feat:feign转发请求头

---
+++ xinmusic-feign/src/main/java/com/ego14t/xinmusicfeign/config/FeignConfig.java
@@ -0,0 +1,34 @@
+package com.ego14t.xinmusicfeign.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+@Configuration
+public class FeignConfig implements RequestInterceptor {
+
+    /**
+     * feign转发请求头,dd
+     * @param requestTemplate
+     */
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder
+                .getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        if (headerNames != null) {
+            while (headerNames.hasMoreElements()) {
+                String name = headerNames.nextElement();
+                String values = request.getHeader(name);
+                requestTemplate.header(name, values);
+
+            }
+        }
+    }
+}
