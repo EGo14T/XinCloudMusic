9d7d6f3880d0bbaff44e2baccb4017d4a1ff53fe
EGo14T <974412906@qq.com>
2020-10-16 18:27:00 +0800

    将mapper.xml放到resource目录下 修改yml配置

--- xinmusic-comments/src/main/java/com/ego14t/comments/CommentsApplication.java
+++ xinmusic-comments/src/main/java/com/ego14t/comments/CommentsApplication.java
@@ -1,13 +1,14 @@
 package com.ego14t.comments;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.ego14t.comments.mapper")
 public class CommentsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CommentsApplication.class, args);
     }
